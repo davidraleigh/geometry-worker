fe5bab89f6c695e7674764c9977fc3ab6fa6c0c0
David Raleigh <davidraleigh@gmail.com>
2015-11-17 19:10:16 +0100

    fix for proximity

--- geometry-server/GeometryHub.cs
+++ geometry-server/GeometryHub.cs
@@ -4,35 +4,50 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using com.esri.core.geometry;
 
 namespace geometry_server {
     public class GeometryHub : Hub {
-        public void Send(string name, string message) {
+
+
+        public void Request(string requestId, string requestContent) {
             // Call the broadcastMessage method to update clients.
-            Clients.All.broadcastMessage(name, message);
+            Clients.Caller.broadcastMessage(requestId, requestContent);
+            Newtonsoft.Json.Linq.JObject jobject = new Newtonsoft.Json.Linq.JObject(new JProperty("RequestId", requestId));
+            try {
+                OperatorCursor geomOpParts = JsonConvert.DeserializeObject<OperatorCursor>(requestContent);
+                double distance = Double.NegativeInfinity;
+                bool spatialRelationship = false;
+                List<Proximity2DResult> proximityResults = null;
 
-            OperatorCursor geomOpParts = JsonConvert.DeserializeObject<OperatorCursor>(message);
-            double distance = Double.NegativeInfinity;
-            bool spatialRelationship = false;
-            List<Proximity2DResult> proximityResults = null;
-
-            GeometryCursor geomCursor = geomOpParts.GenerateCursor(ref distance, ref spatialRelationship, ref proximityResults);
-            if (geomCursor != null) {
-                Geometry geom = null;
-                while ((geom = geomCursor.Next()) != null) {
-                    String wkt = JsonConvert.SerializeObject(GeometryEngine.GeometryToWkt(geom, 0));
-                    Clients.All.broadcastMessage(String.Format("user:{0}, result:{1}", name, wkt));
+                GeometryCursor geomCursor = geomOpParts.GenerateCursor(ref distance, ref spatialRelationship, ref proximityResults);
+                if (geomCursor != null) {
+                    Geometry geom = null;
+                    while ((geom = geomCursor.Next()) != null) {
+                        jobject = new JObject(
+                                new JProperty("RequestId", requestId),
+                                new JProperty("Results", GeometryEngine.GeometryToWkt(geom, 0))
+                            );
+                        Clients.Caller.broadcastMessage(JsonConvert.SerializeObject(jobject));
+                    }
+                    return;
+                } else if (proximityResults != null) {
+                    foreach (Proximity2DResult prox in proximityResults) {
+                        jobject = new JObject( new JProperty("RequestId", requestId),
+                            new JProperty("Results", JsonConvert.SerializeObject(prox)));
+                        Clients.Caller.broadcastMessage(JsonConvert.SerializeObject(jobject));
+                    }
+                    return;
+                } else if (!Double.IsNegativeInfinity(distance)) {
+                    jobject.Add("Results", distance);
+                } else {
+                    jobject.Add("Results", spatialRelationship);
                 }
-            } else if (proximityResults != null) {
-                foreach (Proximity2DResult prox in proximityResults) {
-                    Clients.All.broadcastMessage(String.Format("{user:{0}, result:{1}}", name, JsonConvert.SerializeObject(prox)));
-                }
-            } else if (!Double.IsNegativeInfinity(distance)) {
-                Clients.All.broadcastMessage(String.Format("{user:{0}, result:{1}}", name, distance));
-            } else {
-                Clients.All.broadcastMessage(String.Format("{user:{0}, result:{1}}", name, spatialRelationship));
+            } catch (Exception e) {
+                jobject.Add("Error", e.Message);
             }
+            Clients.Caller.broadcastMessage(JsonConvert.SerializeObject(jobject));
         }
     }
 }
