8be87a7d85ab575800c0215849b76015f93b6235
David Raleigh <davidraleigh@gmail.com>
2015-11-17 14:57:20 +0100

    adding libs

--- geometry-server-cs/GeomOp.cs
+++ geometry-server-cs/GeomOp.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using Newtonsoft.Json;
 using WebSocketSharp;
 using WebSocketSharp.Server;
+using com.esri.core.geometry;
 
 namespace geometry_server_cs {
 	public class GeomOp : WebSocketBehavior {
@@ -35,9 +37,27 @@ namespace geometry_server_cs {
 		}
 
 		protected override void OnMessage(MessageEventArgs e) {
-			GeometryOperator geomOpParts = JsonConvert.DeserializeObject<GeometryOperator>(e.Data);
-			String result = geomOpParts.ExecuteOperator();
-			this.Context.WebSocket.Send(String.Format("{0}: {1}", m_name, result));
+			OperatorCursor geomOpParts = JsonConvert.DeserializeObject<OperatorCursor>(e.Data);
+			double distance = Double.NegativeInfinity;
+			bool spatialRelationship = false;
+			List<Proximity2DResult> proximityResults = null;
+
+			GeometryCursor geomCursor = geomOpParts.GenerateCursor(ref distance, ref spatialRelationship, ref proximityResults);
+			if (geomCursor != null) {
+				Geometry geom = null;
+				while ((geom = geomCursor.Next()) != null) {
+					String wkt = JsonConvert.SerializeObject(GeometryEngine.GeometryToWkt(geom, 0));
+					this.Context.WebSocket.Send(String.Format("user:{0}, result:{1}", m_name, wkt));
+				}
+			} else if (proximityResults != null) {
+				foreach (Proximity2DResult prox in proximityResults) {
+					this.Context.WebSocket.Send(String.Format("{user:{0}, result:{1}}", m_name, JsonConvert.SerializeObject(prox)));
+				}
+			} else if (!Double.IsNegativeInfinity(distance)) {
+				this.Context.WebSocket.Send(String.Format("{user:{0}, result:{1}}", m_name, distance));
+			} else {
+				this.Context.WebSocket.Send(String.Format("{user:{0}, result:{1}}", m_name, spatialRelationship));
+			}
 		}
 	}
 }
