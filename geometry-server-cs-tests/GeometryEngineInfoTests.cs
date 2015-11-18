7ab39ab2e90259eea5a9ed779fe776864fa6a5e5
David Raleigh <davidraleigh@gmail.com>
2015-11-18 14:00:37 +0100

    adding tests

---
+++ geometry-server-cs-tests/GeometryEngineInfoTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using com.esri.core.geometry;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using geometry_server;
+
+namespace geometry_server_cs_tests {
+    [TestClass]
+    public class GeometryEngineInfoTests {
+        [TestMethod]
+        public void EngineMethods() {
+            List<String> enumStrings = GeometryEngineInfo.GeometryEngineMethods();
+            Assert.IsNotNull(enumStrings);
+            Assert.IsFalse(enumStrings.Count == 0);
+            Assert.IsTrue(enumStrings.Contains("Buffer"));
+        }
+
+        [TestMethod]
+        public void MethodDetails() {
+            List<String> methodDetails = GeometryEngineInfo.OperatorInfo("Buffer");
+            Assert.IsNotNull(methodDetails);
+            Assert.IsFalse(methodDetails.Count == 0);
+
+        }
+    }
+}
---
+++ geometry-server/GeometryEngineInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using com.esri.core.geometry;
+
+namespace geometry_server {
+    public class GeometryEngineInfo {
+
+        public static List<String> GeometryEngineMethods() {
+            List<String> geometryOperators = new List<String>();
+            foreach (Operator.Type foo in Enum.GetValues(typeof(Operator.Type))) {
+                geometryOperators.Add(Enum.GetName(typeof(Operator.Type), foo));
+            }
+            //System.Reflection.MethodInfo[] methodInfo = typeof(OperatorBuffer).GetMethods();
+            //System.Reflection.MethodInfo[] mb = typeof(OperatorBufferLocal).GetMethods();
+            return geometryOperators;
+        }
+
+        public static List<String> OperatorInfo(String operatorTypeEnum) {
+            Operator.Type operatorType = (Operator.Type)Enum.Parse(typeof(Operator.Type), operatorTypeEnum);
+            System.Reflection.MethodInfo[] methodInfo = null;
+            if (Enum.IsDefined(typeof(Operator.Type), operatorType) | operatorType.ToString().Contains(",")) {
+                methodInfo = typeof(OperatorBuffer).GetMethods();
+            } else {
+                throw new Exception(String.Format("{0} is not an operator defined in the Operator.Type enum", operatorTypeEnum));
+            }
+
+            List<String> operatorInfo = new List<String>();
+            methodInfo.Where(mi => mi.Name == "Execute")
+                .ToList()
+                .ForEach(mi => operatorInfo.Add(mi.Name));
+            return operatorInfo;
+        }
+
+    }
+}
