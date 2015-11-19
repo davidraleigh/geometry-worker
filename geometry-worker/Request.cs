d9498edce631ba26fd06e2f4146a5d146e007ad8
David Raleigh <davidraleigh@gmail.com>
2015-11-19 16:06:29 +0100

    rabbitmq

---
+++ geometry-worker/Request.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geometry_worker {
+    public class RabbitClusterAzure {
+        public const string ConnectionString =
+            @"host=geometry-mq.cloudapp.net;username=geometry-cs;password=testpassword";
+    }
+
+    class Request {
+        public string Text { get; set; } 
+    }
+}
